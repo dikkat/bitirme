7f21b7be7661145c30e543b5e5561b72e353219c
dikkat <dikkat_albay@hotmail.com>
2020-02-01 10:41:25 +0300

    building testing tools

--- bpv2/bpv2.cpp
+++
@@ -1,7 +0,0 @@
-#include "bpv2.h"
-
-bpv2::bpv2(QWidget *parent)
-	: QMainWindow(parent)
-{
-	ui.setupUi(this);
-}
--- bpv2/main.cpp
+++ bpv2/main.cpp
@@ -1,10 +1,16 @@
-#include "bpv2.h"
+#include "MainWindow.h"
+#include "TestingOperations.h"
 #include <QtWidgets/QApplication>
+#include <QDebug>
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
+	test::tout << "xawedxawehello world" << std::endl;
+	test::printTesting(test::tout);
 	QApplication a(argc, argv);
-	bpv2 w;
+	MainWindow w;
 	w.show();
 	return a.exec();
 }

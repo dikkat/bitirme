f0bf91de8ee91a37e209afa8e609deac4cd69d56
dikkat <dikkat_albay@hotmail.com>
2020-02-09 19:49:17 +0300

    more histogram tools

--- bpv2/Image.cpp
+++ bpv2/Image.cpp
@@ -2,7 +2,22 @@
 
 img::Image::Image(std::string imgdir, int flag) {
 	imagemat = readImageFile(imgdir, flag);
-    Histogram xd(imagemat);
+}
+
+img::Image::Image(cv::Mat sourcemat) {
+    imagemat = sourcemat.clone();
+}
+
+cv::Mat img::Image::getImageMat() {
+    return this->imagemat;
+}
+
+void img::Image::setImageHist(int hb, int sb) {
+    imagehist = new Histogram(this, hb, sb);
+}
+
+img::Histogram* img::Image::getImageHist() {
+    return imagehist;
 }
 
 cv::Mat img::Image::readImageFile(std::string imagedirectory, int flag) {
@@ -34,26 +49,49 @@ std::vector<cv::Mat> img::Image::readImageFolder(std::string imagefolderdirector
 	return vecoperator;
 }
 
-img::Histogram::Histogram(cv::Mat sourcemat) {
-    histmat = histogramCalculation(sourcemat);
-    nhistmat = histogramNormalizedCalculation(histmat);
-    histBGR = histogramBGRCalculation(sourcemat);
+img::Histogram::Histogram(Image* srimg, int hb, int sb) {
+    srcimg = *srimg;
+    hbin = hb;
+    sbin = sb;
+    histmat = histogramCalculation(getSourceMat());
+    nhistmat = normalizeMat(histmat);
+}
+
+cv::Mat img::Histogram::getHistogramMat() {
+    return histmat;
+}
+
+cv::Mat img::Histogram::getSourceMat() {
+    return srcimg.getImageMat();
+}
+
+void img::Histogram::setHistogramDisplayImage() {
+    histBGR = histogramBGRCalculation(getSourceMat());
+    histdspimg = createHistogramDisplayImage(histBGR);
+    cv::namedWindow("adfasdfaxd", cv::WINDOW_AUTOSIZE);
+    cv::imshow("adfasdfaxd", histdspimg.getImageMat());
 }
 
 cv::Mat img::Histogram::histogramCalculation(cv::Mat sourcemat) {
-    int histSize = 256;
-    float range[] = { 0, 256 };
-    const float* histRange = { range };
+    cv::MatND hsvbase, histbase;
+    cv::cvtColor(sourcemat, hsvbase, cv::COLOR_BGR2HSV);
+    int h_bins = hbin, s_bins = sbin;
+    int histSize[] = { h_bins, s_bins};
+
+    float h_ranges[] = { 0, 180 };
+    float s_ranges[] = { 0, 256 };
+    const float* histRange[] = { h_ranges, s_ranges};
+
     bool uniform = true; bool accumulate = false;
-    int channels[] = { 0, 1 };
-    cv::Mat hist;
-    cv::calcHist(&sourcemat, 1, channels, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
-    return hist;
+    int channels[] = { 0, 1};
+
+    cv::calcHist(&hsvbase, 1, channels, cv::Mat(), histbase, 2, histSize, histRange, uniform, accumulate);
+    return histbase;
 }
 
-cv::Mat img::Histogram::histogramNormalizedCalculation(cv::Mat sourcemat) {
+cv::Mat img::Histogram::normalizeMat(cv::Mat sourcemat) {
     cv::Mat nhist;
-    normalize(histmat, nhist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    normalize(sourcemat, nhist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
     return nhist;
 }
 
@@ -64,25 +102,36 @@ std::vector<cv::Mat> img::Histogram::histogramBGRCalculation(cv::Mat sourcemat) 
     int histSize = 256;
     float range[] = { 0, 256 };
     const float* histRange = { range };
-    bool uniform = true; bool accumulate = false;    
+    bool uniform = true; bool accumulate = false;
 
     cv::Mat b_hist, g_hist, r_hist;
     cv::calcHist(&bgr_planes[0], 1, 0, cv::Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
     cv::calcHist(&bgr_planes[1], 1, 0, cv::Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
     cv::calcHist(&bgr_planes[2], 1, 0, cv::Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
 
+    std::vector<cv::Mat> BGR_hist;
+    BGR_hist.push_back(b_hist);
+    BGR_hist.push_back(g_hist);
+    BGR_hist.push_back(r_hist);
+    return BGR_hist;
+}
 
+img::Image img::Histogram::createHistogramDisplayImage(std::vector<cv::Mat> bgrhist){
+    int histSize = 256;
     int hist_w = 512; int hist_h = 400;
     int bin_w = cvRound((double)hist_w / histSize);
 
     cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0, 0, 0));
 
-    normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat()); //FIND OUT THE DIFFERENCE BETWEEN HISTIMAGE.ROWS=400 AND 1
+    cv::Mat b_hist = bgrhist[0];
+    cv::Mat g_hist = bgrhist[1];
+    cv::Mat r_hist = bgrhist[2];
+
+    normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
     normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
     normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
-
-    for (int i = 1; i < histSize; i++)
-    {
+    
+    for (int i = 1; i < histSize; i++){
         line(histImage, cv::Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
             cv::Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
             cv::Scalar(255, 0, 0), 2, 8, 0);
@@ -93,7 +142,8 @@ std::vector<cv::Mat> img::Histogram::histogramBGRCalculation(cv::Mat sourcemat) 
             cv::Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
             cv::Scalar(0, 0, 255), 2, 8, 0);
     }
-    return bgr_planes;
+    Image imoperator(histImage);
+    return imoperator;
 }
 
 
--- bpv2/main.cpp
+++ bpv2/main.cpp
@@ -9,8 +9,26 @@ int main(int argc, char *argv[])
 	gen::tout << "xawedxawehello world" << std::endl;
 
 	try { 
-		img::Image losessomeonedear("C:/ukbench00008.jpg", cv::IMREAD_COLOR); 
+		img::Image losessomeonedear("C:/ukbench00008.jpg", cv::IMREAD_COLOR);
+		losessomeonedear.setImageHist(179, 255);
+		losessomeonedear.getImageHist()->setHistogramDisplayImage();
+
+		cv::Mat xde = losessomeonedear.getImageMat();
+		cv::uint8_t* pixelPtr = (uint8_t*)xde.data;
+		int cn = xde.channels();
+		cv::Scalar_<uint8_t> bgrPixel;
+
+		for (int i = 0; i < xde.rows; i++)
+		{
+			for (int j = 0; j < xde.cols; j++)
+			{
+				bgrPixel.val[0] = pixelPtr[i * xde.cols * cn + j * cn + 0]; // B
+				bgrPixel.val[1] = pixelPtr[i * xde.cols * cn + j * cn + 1]; // G
+				bgrPixel.val[2] = pixelPtr[i * xde.cols * cn + j * cn + 2]; // R
+			}
+		}
 	}
+
 	catch (std::exception e) {
 		gen::tout << e.what();
 	}
